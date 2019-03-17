16785752c62c7037b37bf940e1be52b31d70a4c6
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-03-17 04:34:48 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-student= Student.create()
+student= Student.create(first_name:'Amy',last_name:'Dogg')
